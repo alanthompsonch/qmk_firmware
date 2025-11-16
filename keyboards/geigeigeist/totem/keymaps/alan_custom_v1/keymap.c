45136250e1fa8bdc343a4056eccbafaf838c7a0a
Alan <alan.thompson1995@gmail.com>
2025-11-16 14:52:50 +0100

    fixed stray keycodes and made j-key on num layer transparent

--- keyboards/geigeigeist/totem/keymaps/alan_custom_v1/keymap.c
+++ keyboards/geigeigeist/totem/keymaps/alan_custom_v1/keymap.c
@@ -91,7 +91,6 @@ const uint16_t PROGMEM right_ind_mid_num[] = {KC_EQL, KC_COMM, COMBO_END};
 const uint16_t PROGMEM right_ind_mid_base[] = {KC_G, KC_COMM, COMBO_END};
 const uint16_t PROGMEM left_top_row[] = {KC_Y, KC_U, KC_A, COMBO_END};
 
-KC_EQL,   KC_COMM,
 combo_t key_combos[] = {
     COMBO(left_ind_mid, KC_ESC),
     COMBO(right_ind_mid_num, KC_ENT),
@@ -148,7 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
                      KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F11,   KC_F12,   KC_F9,    KC_F10,
                      KC_1,  LALT_T(KC_2),KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
-          _______,   KC_QUOT,  XXXXXXX,  _______,  _______,  KC_LBRC,   KC_RBRC,  KC_EQL,   KC_COMM,  KC_DOT,   KC_COLN,   KC_BSPC,
+          _______,   KC_QUOT,  _______,  _______,  _______,  KC_LBRC,   KC_RBRC,  KC_EQL,   KC_COMM,  KC_DOT,   KC_COLN,   KC_BSPC,
                                          _______,  _______,  _______,   _______,  _______,  _______
     ),
 
